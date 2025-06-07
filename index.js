304c8b53991160b56e786e3fceda6d8f5250534a
Akhil Prasanna <akhilsann@gmail.com>
2025-06-07 10:27:33 -0400

    Updated the chat UI

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ let send = document.getElementById("send");
 let chat = document.getElementById("chat");
 let message = document.getElementById("message");
 
-
 send.addEventListener("click", function() {
+    if (message.value != ""){
     chat.innerHTML = chat.innerHTML + "\n" + message.value;
     chat.style.whiteSpace = "pre-line";
+    message.value = '';
+    }
 })
+
+
